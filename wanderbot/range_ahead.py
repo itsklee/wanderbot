dd02ef7d0a1b67125687547281ae63ecf8b381bb
Seok kyu Lee <itsklee@gmail.com>
2020-11-20 23:18:40 +0900

    LaseScan Received  and Well wander in turtlebot3

--- wanderbot/range_ahead.py
+++ wanderbot/range_ahead.py
@@ -3,6 +3,7 @@
 import rclpy
 
 from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
 from sensor_msgs.msg import LaserScan
 
 class Range_ahead(Node):
@@ -14,7 +15,7 @@ class Range_ahead(Node):
             LaserScan,
             '/scan',
             self.scan_callback,
-            100)
+            qos_profile=qos_profile_sensor_data)
         self.subscription  # prevent unused variable warning
         self.get_logger().info('Init OK')
 
@@ -22,7 +23,7 @@ class Range_ahead(Node):
     def scan_callback(self,  msg):
         self.get_logger().info('scan callback called')
         if( len(msg.ranges) > 0):
-            self.range_ahead = msg.ranges[len(msg.ranges)/2]
+            self.range_ahead = msg.ranges[int(len(msg.ranges)/2)]
             self.get_logger().info('range ahead: %0.1f' % self.range_ahead)
         else:
             self.get_logger().info('msg.angle_min %0.1f msg.angle_max %0.1f msg.angle_increment %0.1f \
--- wanderbot/wander.py
+++ wanderbot/wander.py
@@ -3,9 +3,9 @@
 import rclpy
 
 from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-
 
 class Wander(Node):
 
@@ -19,7 +19,7 @@ class Wander(Node):
             LaserScan,
             '/scan',
             self.scan_callback,
-            10)
+            qos_profile=qos_profile_sensor_data)
 
         self.timer_period = 0.1  # seconds
         self.time = self.create_timer(
@@ -27,38 +27,74 @@ class Wander(Node):
             self.timer_callback)
         self.i = 0
 
-        self.g_range_ahead = 1
+        self.FRONT = 0
+        self.RIGHT = 1
+        self.BACK = 2
+        self.LEFT = 3
+        self.WALLDISTANCE  = 0.8
+
+        self.g_range_ahead = [0.,0.,0.,0.]
         self.driving_forward = True
-        self.state_change_time = self.timer_duration(5)
+        self.state_change_time = 0
         self.get_logger().info('Init OK')
 
     def scan_callback(self,  msg):
         if( len(msg.ranges) > 0):
-            self.g_range_ahead = min(msg.ranges)
-        self.get_logger().info('range min: %0.1f' % self.g_range_ahead)
+            self.g_range_ahead = [min(min(msg.ranges[0:44]),min(msg.ranges[315:359])), # FRONT
+            min(msg.ranges[45:134]), # RIGHT 
+            min(msg.ranges[135:224]), # BACK
+            min(msg.ranges[225:314])] # LEFT
+
+#            self.g_range_ahead = min(msg.ranges)
+#        self.get_logger().info('range min: %0.1f' % self.g_range_ahead)
 
 
     def timer_callback(self):
+        bchange = False
         if self.driving_forward:
-            if ( self.g_range_ahead < 0.8 or self.timer_now() >= self.state_change_time):
+            if ( self.g_range_ahead[self.FRONT] < self.WALLDISTANCE or self.timer_now() >= self.state_change_time ):
                 self.driving_forward = False
-                self.state_change_time = self.timer_now() + self.timer_duration(5)
+                self.state_change_time = self.timer_now() + self.timer_duration(1)
+                bchange = True
         else :
             if self.timer_now() >= self.state_change_time:
                 self.driving_forward = True
-                self.state_change_time = self.timer_now() +  self.timer_duration(10)
+                self.state_change_time = self.timer_now() +  self.timer_duration(30)
+                bchange = True
 
         twist = Twist()
         if self.driving_forward:
-            twist.linear.x = 0.5
+            twist.linear.x = 0.3
         else:
-            twist.linear.x = 0.5
-            twist.angular.z = 0.5
+            if (self.g_range_ahead[self.LEFT] > self.g_range_ahead[self.RIGHT]):
+                if ( self.g_range_ahead[self.LEFT] > self.WALLDISTANCE ):
+                    twist.angular.z = -0.3
+                    twist.linear.x = -0.01
+                else:
+                    if (self.g_range_ahead[self.BACK] > self.WALLDISTANCE):
+                        twist.linear.x = -0.3
+                        twist.angular.z = 0.0
+                    else :
+                        twist.linear.x = -0.01
+                        twist.angular.z = 0.0
+            else  :
+                if ( self.g_range_ahead[self.RIGHT] > self.WALLDISTANCE):
+                    twist.angular.z = 0.3
+                    twist.linear.x = -0.01
+                else:
+                    if (self.g_range_ahead[self.BACK] > self.WALLDISTANCE):
+                        twist.linear.x = -0.3
+                        twist.angular.z = 0.0
+                    else :
+                        twist.linear.x = -0.01
+                        twist.angular.z = 0.0
+
         
         self.cmd_vel_pub.publish(twist)
-              
-        self.get_logger().info('Publishing: "Direction %d  Time %d Speed %0.1f Angle %0.1f"' 
-        % (self.driving_forward,self.i,twist.linear.x,twist.angular.z))
+
+        if bchange :
+            self.get_logger().info('Publishing: "D %d S %0.1f A %0.1f RF %0.1f RL %0.1f RB %0.1f RR %0.1f"' 
+            % (self.driving_forward,twist.linear.x,twist.angular.z,self.g_range_ahead[0],self.g_range_ahead[1],self.g_range_ahead[2],self.g_range_ahead[3] ))
         self.i += 1
 
     def timer_now(self):
